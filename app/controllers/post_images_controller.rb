45e6271583a0d7bd0397c6d403bce6c7b37ec1f7
EC2 Default User <ec2-user@ip-172-31-34-211.eu-north-1.compute.internal>
2024-11-19 10:51:30 +0000

    first commmit

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,9 +12,9 @@ class PostImagesController < ApplicationController
       render :new
     end
   end
-  
+
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
